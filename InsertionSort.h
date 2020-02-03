d0b0844097d83266c15dd8087358446ecbdd1671
Hattie12 <43356148+Hattie12@users.noreply.github.com>
2020-02-02 23:26:34 -0500

    Create InsertionSort.h

---
+++ InsertionSort.h
@@ -0,0 +1,11 @@
+#ifndef _INSERTIONSORT_H_
+#define _INSERTIONSORT_H_
+#include <vector>
+#include <fstream>
+
+//sorts reference vec based on insertion sort method
+//returns number of comparisons made in the process
+long long InsertionSort(std::vector<int> &a); //do not change this line 
+
+
+#endif

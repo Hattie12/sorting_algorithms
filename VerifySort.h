df26989a2d31d44ff6282276ee53190f5f7887f1
Hattie12 <43356148+Hattie12@users.noreply.github.com>
2020-02-02 23:27:13 -0500

    Create VerifySort.h

---
+++ VerifySort.h
@@ -0,0 +1,12 @@
+#ifndef _VERIFYSORT_H_
+#define _VERIFYSORT_H_
+#include <vector>
+#include <fstream>
+
+
+//verifies that vec is actually sorted
+//returns t/f depending
+bool VerifySort(const std::vector<int> &a); 
+
+
+#endif
